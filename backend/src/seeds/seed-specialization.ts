1d0bd9f8e6d214037622da1fb095266d3023cc57
Danilo Jovanović <jovanovic.danilo06@gmail.com>
2025-05-22 22:27:49 +0200

    Specializations get all route implemented

---
+++ backend/src/seeds/seed-specialization.ts
@@ -0,0 +1,27 @@
+import { PutCommand } from "@aws-sdk/lib-dynamodb";
+import { dbDocClient } from "../database/dynamodb.service";
+
+const specializations = [
+  { id: "000001", specialization: "React" },
+  { id: "000002", specialization: "Angular" },
+  { id: "000003", specialization: "Express" },
+  { id: "000004", specialization: "NodeJS" },
+  { id: "000005", specialization: "Java" },
+  { id: "000006", specialization: "C++" },
+
+];
+
+async function seedspecializations() {
+  for (const specialization of specializations) {
+    await dbDocClient.send(
+      new PutCommand({
+        TableName: "Specializations",
+        Item: specialization,
+      })
+    );
+    console.log(`Inserted training type: ${specialization.specialization}`);
+  }
+  console.log("Seeding complete.");
+}
+
+seedspecializations().catch(console.error);
---
+++ backend/src/specialization/specialization.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from "@nestjs/common";
+import { SpecializationsService } from "./specialization.service";
+
+@Controller('specializations')
+export class SpecializationsController {
+    constructor(private readonly specializationsService: SpecializationsService) {}
+
+    @Get('/')
+    async getAll() {
+        return this.specializationsService.getAll();
+    }
+}
